b5f88d2a4c6848075eb4d6ef6b14aa630f40650e
dmitryyyan <dima2003yan@gmail.com>
2024-12-17 14:13:15 +0300

    new

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,33 @@
 import { defineConfig } from 'vite';
-import glob from 'glob';
+import fg from 'fast-glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+import path from 'path';
 
 export default defineConfig(({ command }) => {
+  // Перевірка значення командної опції
+  const isServe = command === 'serve';
+
   return {
     define: {
-      [command === 'serve' ? 'global' : '_global']: {},
+      [isServe ? 'global' : '_global']: {},
     },
     root: 'src',
     build: {
       sourcemap: true,
-
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: fg.sync('./src/*.html').map(file => path.resolve(file)),
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
               return 'vendor';
             }
           },
-          entryFileNames: 'commonHelpers.js',
+          entryFileNames: '[name].[hash].js',
         },
       },
       outDir: '../dist',
     },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    plugins: [injectHTML(), FullReload(['./src/**/*.html', './src/**/*.js'])],
   };
 });
